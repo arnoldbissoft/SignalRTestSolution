df7f3ccd6a4069771a38425bf3e2fb98c679f89f
Arnold <arnoldk@bissoft.ro>
2020-01-09 19:22:41 +0200

    Add project files.

---
+++ SignalRTestApp/wwwroot/js/chat.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message, rate) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg + " with a rate of " + rate;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    var rate = document.getElementById("rateInput").value;
+    connection.invoke("SendMessage", user, message, rate).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
